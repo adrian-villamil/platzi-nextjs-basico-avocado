3d0a576124f50e181142d19e7d4ac0702eadbe58
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-01-23 08:45:01 -0500

    Organized its content, besides the linkes open the urls in new tabs.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import styles from './footer.module.css';
-import { FaInstagram, FaGithub } from 'react-icons/fa';
 import Link from 'next/link';
+import { FaLinkedin } from 'react-icons/fa';
+import styles from './footer.module.css';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>Find me on social media.</p>
       <p className={styles.smIcons}>
-        <Link href='https://www.instagram.com/adrian_villamil_art/'>
-          <FaInstagram />
-        </Link> 
+        <Link
+          href='https://www.linkedin.com/in/adrian-felipe-villamil-arias-260736222/'
+          target='_blank'
+        >
+          <FaLinkedin />
+        </Link>
       </p>
-      <p>Powered by <Link className={styles.linkGithub} href='https://github.com/adrian-villamil'>Adrian Villamil</Link></p>
+      <p>
+        {'Powered by '}
+        <Link
+          className={styles.linkGithub}
+          href='https://github.com/adrian-villamil'
+          target='_blank'
+        >
+          Adrian Villamil
+        </Link>
+      </p>
     </footer>
   );
 };
